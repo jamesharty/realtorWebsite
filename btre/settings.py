9bf6b185cc263ed36b7762721df8053a4d0c17ef
jamesharty <jharty11@gmail.com>
2019-10-11 22:36:25 +0100

    Added listing model

--- btre/settings.py
+++ btre/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASWWORD': '1234',
+        'PASSWORD': '1234',
         'HOST':'localhost'
     }
 }
